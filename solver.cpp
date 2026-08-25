c75201f4a31bc54d7b9149571bf72fa4a45ea501
Jamie Landolt <17292835+jamie-landolt@user.noreply.gitee.com>
2026-08-25 20:09:24 +1000

    bmark-5: remove internal solver debug prints, keep benchmark output only

--- solver.cpp
+++ solver.cpp
@@ -334,8 +334,6 @@ public:
 		auto dfs_start_time = std::chrono::steady_clock::now();
 		long long dfs_iterations = 0;
 		for (int search_depth = 1; search_depth <= MAX_DEPTH; search_depth++) {
-			std::cout << "Searching Depth: " << search_depth << "\n";
-
 			// Reset dfs state
 			reset_dfs(scramble);
 
@@ -380,12 +378,6 @@ public:
 					move_space = DOMINO_MOVES;
 					search_depth = 1;
 					MAX_DEPTH = DEPTH_PHASE_2;
-
-					std::cout << "Domino reduction complete with: ";
-					for (std::string s : state_moves) {
-						std::cout << s << ", ";
-					}
-					std::cout << "\n";
 
 					// Save moves to get to that state
 					solution.first = state_moves;
